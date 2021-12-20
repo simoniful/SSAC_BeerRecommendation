2e028c523cdc6ac0a9ed7ecb0d34beeae5f07e10
sanghunlee <simoniful0gmail.com>
2021-12-20 13:58:45 +0900

    Initial Commit

---
+++ SSAC_BeerRecommendation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_BeerRecommendation
+//
+//  Created by Sang hun Lee on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
